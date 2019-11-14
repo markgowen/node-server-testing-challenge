be9f85c3d3e5576f6f6d589deff757f4ef0a8a4b
Mark Gowen <mark.gowen78@gmail.com>
2019-11-14 12:36:37 -0800

    created insert and remove routes in server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,33 @@
 const express = require("express");
 
+const Users = require('../api/models/user-model');
+
 const server = express();
 
 server.use(express.json());
+
+server.post('/', (req, res) => {
+    Users
+        .insert(req.body)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+server.delete('/:id', (req, res) => {
+    Users
+        .remove(req.params)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+
 
 module.exports = server;
