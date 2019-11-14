4b9dc842ed57f314b4e05fafbc01c05ed7302f2c
Mark Gowen <mark.gowen78@gmail.com>
2019-11-14 12:24:59 -0800

    inital project setup

---
+++ api/models/user-model.js
@@ -0,0 +1,19 @@
+const db = require("../../data/dbConfig");
+
+module.exports = {
+  insert,
+  remove,
+  findById
+};
+
+async function insert(user) {
+  return null;
+}
+
+async function remove(id) {
+  return null;
+}
+
+async function findById(id) {
+  return null;
+}
